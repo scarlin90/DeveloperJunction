098b84832ade59d24b207531ca460bf2c41b6b2f
Colum Ferry <cferry@smashfly.com>
2019-07-04 10:10:14 +0100

    Close #4 Add DAL abstractions

---
+++ src/app/shared/data-access/entity.service.interface.ts
@@ -0,0 +1,24 @@
+import { Entity } from './entity.interface';
+import { Observable } from 'rxjs';
+
+export interface IEntityService<T extends Entity> {
+    get(id: string): T;
+    getAsync(id: string): Observable<T>;
+    add(entity: T): Promise<void>;
+    update(updatedEntity: T): Promise<void>;
+    delete(id: string): Promise<void>;
+}
+
+export abstract class EntityService<T extends Entity> implements IEntityService<T> {
+    private storeName: string;
+
+    constructor(storeName: string) {
+        this.storeName = storeName;
+    }
+
+    abstract get(id: string): T;
+    abstract getAsync(id: string): Observable<T>;
+    abstract add(entity: T): Promise<void>;
+    abstract update(updatedEntity: T): Promise<void>;
+    abstract delete(id: string): Promise<void>;
+}
