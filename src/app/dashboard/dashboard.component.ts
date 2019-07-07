fa7fe2e3abe44d021099cc501407239a73c9361f
Sean Carlin <scarlin@smashfly.com>
2019-07-07 20:45:37 +0100

    Replacing fibase auth with FirebaseUI abstraction

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { AuthenticationService } from '../authentication/authentication.service';
+import { AuthenticationService } from "../authentication/authentication.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"]
 })
-export class DashboardComponent implements OnInit {
-
-  constructor(private  authService:  AuthenticationService) { }
-    ngOnInit() {}
-
+export class DashboardComponent {
+  constructor() {}
 }
