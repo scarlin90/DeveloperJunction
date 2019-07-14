48747a4887da88415a93931312a5e3e139540a4a
Sean Carlin <scarlin@smashfly.com>
2019-07-14 23:27:21 +0100

    Adding Github provider to configuration

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,33 +1,34 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthenticationService } from "../authentication/authentication.service";
-import * as firebaseui from "firebaseui";
-import * as firebase from "firebase";
-import { AngularFireAuth } from "@angular/fire/auth";
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication/authentication.service';
+import * as firebaseui from 'firebaseui';
+import * as firebase from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthenticationService, private afAuth: AngularFireAuth) {}
+    constructor(private authService: AuthenticationService, private afAuth: AngularFireAuth) {}
 
-  ngOnInit() {
-    const authUi = new firebaseui.auth.AuthUI(this.afAuth.auth);
-    const uiConfig: firebaseui.auth.Config = {
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      callbacks: {
-        signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this)
-      }
-    };
+    ngOnInit() {
+        const authUi = new firebaseui.auth.AuthUI(this.afAuth.auth);
+        const uiConfig: firebaseui.auth.Config = {
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                firebase.auth.GithubAuthProvider.PROVIDER_ID
+            ],
+            callbacks: {
+                signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this)
+            }
+        };
 
-    this.authService.createLoginUi("#firebaseui-auth-container", uiConfig, authUi);
-  }
+        this.authService.createLoginUi('#firebaseui-auth-container', uiConfig, authUi);
+    }
 
-  onLoginSuccessful(authResult) {
-    this.authService.login(authResult.user);
-  }
+    onLoginSuccessful(authResult) {
+        this.authService.login(authResult.user);
+    }
 }
